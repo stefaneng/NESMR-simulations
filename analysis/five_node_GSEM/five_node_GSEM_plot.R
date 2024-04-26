5e4641937973c11e619bae3295d3b3ccf7e503dd
Stefan Eng <engs@umich.edu>
2024-04-26 10:45:08 -0400

    Update five node GSEM plots

--- analysis/five_node_GSEM/five_node_GSEM_plot.R
+++ analysis/five_node_GSEM/five_node_GSEM_plot.R
@@ -2,8 +2,8 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-
-merged_results <- read.csv('/nfs/turbo/sph-jvmorr/NESMR/simulations/five_node_GSEM_eval/nesmr_vs_genomic_sem_results.csv') %>%
+results_dir <- "/nfs/turbo/sph-jvmorr/NESMR/simulations/five_node_GSEM_eval/"
+merged_results <- read.csv(file.path(results_dir, "nesmr_vs_genomic_sem_results.csv")) %>%
     mutate(bias = beta - true_beta) %>%
     separate(edge, c('from', 'to'), sep = '->')
 
@@ -16,22 +16,26 @@ merged_results %>%
     arrange(desc(abs(max_bias)))
 
 p1 <- filter(merged_results, n == 40000) %>% ggplot(.) +
-    facet_grid(rows = vars(from), cols = vars(to)) +
+    facet_grid(rows = vars(from), cols = vars(to),
+         labeller = labeller(from = label_both, to = label_both)) +
     xlim(c(-0.5, 0.5)) +
     geom_histogram(aes(x = bias, fill = model), alpha = 0.5, position="identity") +
     ggtitle('N = 40,000') +
-    ylab('') +
+#    ylab('') +
     theme_minimal(base_size = 24) +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+    theme(
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 p2 <- filter(merged_results, n == 10000) %>% ggplot(.) +
-    facet_grid(rows = vars(from), cols = vars(to)) +
+    facet_grid(rows = vars(from), cols = vars(to),
+         labeller = labeller(from = label_both, to = label_both)) +
     xlim(c(-0.5, 0.5)) +
     geom_histogram(aes(x = bias, fill = model), alpha = 0.5, position="identity") +
     ggtitle('N = 10,000') +
-     ylab('') +
+#     ylab('') +
     theme_minimal(base_size = 24) +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+    theme(
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
-ggsave('nesmr_vs_genomic_sem_bias_10000.jpeg', p2, width = 14, height = 14, dpi = 300)
-ggsave('nesmr_vs_genomic_sem_bias_40000.jpeg', p1, width = 14, height = 14, dpi = 300)
+ggsave(file.path(results_dir, "nesmr_vs_genomic_sem_bias_40000.jpeg"), p1, width = 14, height = 14, dpi = 300)
+ggsave(file.path(results_dir, "nesmr_vs_genomic_sem_bias_10000.jpeg"), p2, width = 14, height = 14, dpi = 300)
