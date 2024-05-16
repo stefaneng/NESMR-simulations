9efbf2c18096fd20da0dc1dfcba68fccc15ccdcb
Stefan Eng <engs@umich.edu>
2024-05-16 08:31:16 -0400

    Add winners curse sim

--- R/winners_curse/extract_winners_curse_sim.R
+++ R/winners_curse/extract_winners_curse_sim.R
@@ -15,7 +15,7 @@ extract_lower_triangular <- function(mat, prefix = "") {
     setNames(mat[lower.tri(mat)], low_tri_arrow)
 }
 
-dir <- "/nfs/turbo/sph-jvmorr/NESMR/simulations/2024-05-13_winners_curse"
+dir <- "/nfs/turbo/sph-jvmorr/NESMR/simulations/2024-05-14_winners_curse"
 dscout1 <- dscquery(dsc.outdir = dir,
                    targets    = c(
                     "simulate.scale_effects",
@@ -29,6 +29,11 @@ dscout1 <- dscquery(dsc.outdir = dir,
                     return.type = "list",
                    ignore.missing.files = TRUE)
 
+true_beta <- c(sqrt(0.4), 0, sqrt(0.2))
+
+# remove prefix before "."
+names(dscout1) <- gsub(".*\\.", "", names(dscout1))
+
 valid_sims <- sapply(seq_along(dscout1$cursed_model), function(i) {
     return(class(dscout1$cursed_model[[i]]) == "list" &&
         class(dscout1$ma_SNP_model[[i]]) == "list")
@@ -40,10 +45,20 @@ res <- lapply(seq_along(dscout1$DSC)[valid_sims], function(i) {
     cursed_n_variants <- length(dscout1$cursed_ix[[i]])
     ma_n_variants <- length(dscout1$ma_ix[[i]])
 
+    tmp_true_beta <- true_beta * dscout1$scale_effects[[i]]
+
+    cursed_beta <- t(extract_lower_triangular(cursed_mod$direct_effects, prefix = "cursed_beta_"))
+    ma_beta <- t(extract_lower_triangular(ma_mod$direct_effects, prefix = "ma_beta_"))
+    cursed_bias <- cursed_beta - tmp_true_beta
+    colnames(cursed_bias) <- gsub("beta", "bias", colnames(cursed_beta))
+    ma_bias <- ma_beta - tmp_true_beta
+    colnames(ma_bias) <- gsub("beta", "bias", colnames(ma_beta))
 
     data.frame(
-        t(extract_lower_triangular(cursed_mod$direct_effects, prefix = "cursed_beta_")),
-        t(extract_lower_triangular(ma_mod$direct_effects, prefix = "ma_beta_")),
+        cursed_beta,
+        ma_beta,
+        cursed_bias,
+        ma_bias,
         t(extract_lower_triangular(-cursed_mod$pvals_dm / log(10), prefix = "cursed_log10_pvals_")),
         t(extract_lower_triangular(-ma_mod$pvals_dm / log(10), prefix = "ma_log10_pvals_")),
         cursed_n_variants,
@@ -58,4 +73,4 @@ res <- lapply(seq_along(dscout1$DSC)[valid_sims], function(i) {
 res_merged <- do.call(rbind, res)
 
 today <- format(Sys.Date(), "%Y-%m-%d")
-saveRDS(res_merged, file = print(file.path(dir, paste0(today, "_winners_curse_results.rds"))))
+saveRDS(res_merged, file = print(file.path(dir, paste0(today, "_winners_curse_results2.rds"))))
